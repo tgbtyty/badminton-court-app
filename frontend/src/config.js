95d8e0a0940c29508725e556cc68e358f289ad23
tgbtyty <alexqin777@gmail.com>
2024-08-26 19:07:51 -0400

    morecom

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiBaseUrl: process.env.NODE_ENV === 'production' 
+      ? 'http://134.209.64.243/api'
+      : 'http://localhost:5000/api'
+  };
+  
+  export default config;
