caa8352c334ff7356e9c8f9ae28cc1b00870421c
Alex Qin <alexqin777@gmail.com>
2024-08-26 21:41:56 -0400

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import config from '../config';
 
 function Login() {
   const [username, setUsername] = useState('');
