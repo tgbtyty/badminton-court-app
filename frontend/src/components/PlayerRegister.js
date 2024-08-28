9b472f67327f9cf5441c2f667cf7d9e6e0ca04eb
Alex Qin <alexqin777@gmail.com>
2024-08-27 21:06:27 -0400

    Update PlayerRegister.js

--- frontend/src/components/PlayerRegister.js
+++ frontend/src/components/PlayerRegister.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import config from '../config';
 
 function PlayerRegister() {
   const [firstName, setFirstName] = useState('');
