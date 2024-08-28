d0004b1db1b86f2131319ea93f7a9cc2aefa9377
Alex Qin <alexqin777@gmail.com>
2024-08-27 21:06:02 -0400

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import config from '../config';
 
 function HomePage() {
   const [courts, setCourts] = useState([]);
