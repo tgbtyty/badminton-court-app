d442436951ed07366b6cc2568cfaff592b9fe3d7
Alex Qin <alexqin777@gmail.com>
2024-08-27 21:16:45 -0400

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -13,7 +13,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('${config.apiBaseUrl}/register', { username, password, firstName, lastName });
+      await axios.post(`${config.apiBaseUrl}/register`, { username, password, firstName, lastName });
       alert('Registration successful. Please login.');
       navigate('/login');
     } catch (error) {
