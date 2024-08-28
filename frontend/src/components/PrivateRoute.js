33393e210324510b67b0f1184ce90dacd2c87b93
Alex Qin <alexqin777@gmail.com>
2024-08-27 21:06:51 -0400

    Update PrivateRoute.js

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import config from '../config';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
