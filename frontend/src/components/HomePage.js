869b927ab1d0066e0c683971ef709a7ff3b7a9ae
Alex Qin <alexqin777@gmail.com>
2024-09-03 18:51:28 -0400

    usecallback

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
